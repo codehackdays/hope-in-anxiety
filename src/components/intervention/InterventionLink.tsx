2213f0a5351063661fa6b576c86f58c3d1ac1d7a
Peter Boggis <codehackdays@googlemail.com>
2021-10-16 08:40:34 +0100

    Change help button to more sensitive colour

--- src/components/intervention/InterventionLink.tsx
+++ src/components/intervention/InterventionLink.tsx
@@ -6,7 +6,8 @@ interface ContainerProps { }
 const InterventionLink: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <IonButton color="primary" shape="round" href="/home">Reset</IonButton><IonButton color="danger" shape="round" href="/help">Don't Panic!</IonButton>
+      <IonButton color="primary" shape="round" href="/home">Reset</IonButton>
+      <IonButton color="warning" shape="round" href="/help">Don't Panic!</IonButton>
     </div>
   );
 };
